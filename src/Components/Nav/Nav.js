5616844ce3ab413fa32b2edd059d5802597e11b6
Jake-Norton <jakenorton60@gmail.com>
2021-01-26 15:41:53 -0700

    test

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,7 +4,9 @@ import homeLogo from './../../assets/home_logo.png';
 import newLogo from './../../assets/new_logo.png';
 import logoutLogo from './../../assets/shut_down.png';
 import './Nav.css';
-import {Link, withRouter} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {updateUser, logout} from '../../redux/reducer'
 
 class Nav extends Component {
   constructor(props) {
@@ -29,19 +31,21 @@ class Nav extends Component {
   }
   
   render() {
-      return this.props.location.pathname !== '/' &&
-        <div className='nav'>
-          <div className='nav-profile-container'>
-            <div className='nav-profile-pic'></div>
-            <p>placeholder username</p>
-          </div>
-          <div className='nav-links'>
-            <Link to='/dash' className='links'><img className='nav-img' src={homeLogo} alt='home' /></Link>
-            <Link to='/form' className='links'><img className='nav-img' src={newLogo} alt='new post' /></Link>
-          </div>
-          <Link to='/' className='links' onClick='logout()'><img className='nav-img logout' src={logoutLogo} alt='logout' /></Link>
+    console.log(this.props)
+    return <div className='nav'>
+        <div className='nav-profile-container'>
+         <div className='nav-profile-pic'></div>          
+         <p>placeholder username</p>
         </div>
+        <div className='nav-links'>
+          <Link to='/dash' className='links'><img className='nav-img' src={homeLogo} alt='home' /></Link>
+          <Link to='/form' className='links'><img className='nav-img' src={newLogo} alt='new post' /></Link>
+        </div>
+        <Link to='/' className='links' onClick='logout()'><img className='nav-img logout' src={logoutLogo} alt='logout' /></Link>
+      </div>
   }
 }
 
-export default withRouter(Nav);
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps, {updateUser, logout})(Nav);
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
 import axios from 'axios';
 import noImage from './../../assets/no_image.jpg';
 import './Post.css';
